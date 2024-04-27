b12f17342373a23f717272693d9472b2ba3cdf06
Jonghyo Lee <na1307@outlook.kr>
2024-04-27 12:17:51 +0900

    Test

---
+++ Test/Verify.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+if [ $1 == "--non-main-repo" ]; then
+	MAINREPO=false;
+elif [ $1 == "--main-repo" ]; then
+    MAINREPO=true;
+else
+    echo Unknown Argument.
+    exit 1
+fi
+
+if [ ! -f _out/wwwroot/.nojekyll ]; then
+    echo ".nojekyll doesn't exist!"
+	exit 1
+fi
+
+if [ $MAINREPO == true ]; then
+	grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
+else
+    grep -i "base href=\"/$2/\"" _out/wwwroot/index.html > /dev/null
+fi
