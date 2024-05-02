ed14ddcd726bf178b96eb5f28e287c29a1835aca
Jonghyo Lee <na1307@outlook.kr>
2024-05-02 17:37:36 +0900

    404

--- Test/Verify.sh
+++ Test/Verify.sh
@@ -9,6 +9,15 @@ else
     exit 1
 fi
 
+if [ $2 == "--no-fix-404" ]; then
+    FIX404=false;
+elif [ $2 == "--fix-404" ]; then
+    FIX404=true;
+else
+    echo Unknown Argument.
+    exit 1
+fi
+
 if [ ! -f _out/wwwroot/.nojekyll ]; then
     echo ".nojekyll doesn't exist!"
     exit 1
@@ -17,5 +26,9 @@ fi
 if [ $MAINREPO == true ]; then
     grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
 else
-    grep -i "base href=\"/$2/\"" _out/wwwroot/index.html > /dev/null
+    grep -i "base href=\"/$3/\"" _out/wwwroot/index.html > /dev/null
+fi
+
+if [ $MAINREPO == false ] && [ $FIX404 == true ]; then
+    grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
 fi
