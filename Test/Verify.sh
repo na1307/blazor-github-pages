c735d951a3952615c72bfd0d88d3c4dabc1cc3ac
Jonghyo Lee <na1307@outlook.kr>
2024-12-24 15:04:19 +0900

    Update

--- Test/Verify.sh
+++ Test/Verify.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/bash
 
-if [ $1 == "--main-repo=false" ]; then
-    MAINREPO=false;
-elif [ $1 == "--main-repo=true" ]; then
-    MAINREPO=true;
-else
-    echo Unknown Argument.
-    exit 1
-fi
+#if [ $1 == "--main-repo=false" ]; then
+#    MAINREPO=false;
+#elif [ $1 == "--main-repo=true" ]; then
+#    MAINREPO=true;
+#else
+#    echo Unknown Argument.
+#    exit 1
+#fi
 
-if [ $2 == "--fix-404=false" ]; then
-    FIX404=false;
-elif [ $2 == "--fix-404=true" ]; then
-    FIX404=true;
-else
-    echo Unknown Argument.
-    exit 1
-fi
+#if [ $2 == "--fix-404=false" ]; then
+#    FIX404=false;
+#elif [ $2 == "--fix-404=true" ]; then
+#    FIX404=true;
+#else
+#    echo Unknown Argument.
+#    exit 1
+#fi
 
-if [ ! -f _out/wwwroot/.nojekyll ]; then
-    echo ".nojekyll doesn't exist!"
-    exit 1
-fi
+#if [ ! -f _out/wwwroot/.nojekyll ]; then
+#    echo ".nojekyll doesn't exist!"
+#    exit 1
+#fi
 
-if [ $MAINREPO == true ]; then
-    grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
-else
-    grep -i "base href=\"/$3/\"" _out/wwwroot/index.html > /dev/null
-fi
+#if [ $MAINREPO == true ]; then
+#    grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
+#else
+#    grep -i "base href=\"/$3/\"" _out/wwwroot/index.html > /dev/null
+#fi
 
-if [ $MAINREPO == false ] && [ $FIX404 == true ]; then
-    grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
-else
-    ! grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
-fi
+#if [ $MAINREPO == false ] && [ $FIX404 == true ]; then
+#    grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
+#else
+#    ! grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
+#fi
+
+grep -i "base href=\"/$1/\"" _out/wwwroot/index.html > /dev/null
+! grep -i "/$1/?p=/" _out/wwwroot/404.html > /dev/null
--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,90 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as fs from 'node:fs'
-import repoName from 'git-repo-name';
+import {context} from '@actions/github';
 
 async function main(): Promise<void> {
-  try {
-    const projectPath = core.getInput('project-path');
-    const publishPath = core.getInput('publish-path');
-    const isMainRepo = core.getBooleanInput('main-repo');
-    const fix404 = core.getBooleanInput('fix-404');
-    const dotnet: string = 'dotnet';
+    try {
+        const projectPath = core.getInput('project-path', {required: true, trimWhitespace: true});
+        const publishPath = core.getInput('publish-path');
+        const dotnet: string = 'dotnet';
 
-    core.info('Restoring dependencies...');
-    const restoreResult = await exec.exec(dotnet, [
-      'restore',
-      `"${projectPath}"`
-    ]);
+        core.info(`Project Path: ${projectPath}`);
+        
+        if (!fs.existsSync(projectPath)) {
+            core.setFailed('The project not found.');
+            return;
+        }
 
-    if (restoreResult) {
-      throw new Error('Restore failed.');
+        core.info('Restoring dependencies...');
+        const restoreResult = await exec.exec(dotnet, [
+            'restore',
+            `"${projectPath}"`
+        ]);
+
+        if (restoreResult) {
+            core.setFailed('Restore failed.');
+            return;
+        }
+
+        core.info('Building...');
+        const buildResult = await exec.exec(dotnet, [
+            'build',
+            `"${projectPath}"`,
+            '--no-restore',
+            '-c',
+            'Release'
+        ]);
+
+        if (buildResult) {
+            core.setFailed('Build failed.');
+            return;
+        }
+
+        core.info('Publishing...');
+        const publishResult = await exec.exec(dotnet, [
+            'publish',
+            `"${projectPath}"`,
+            '--no-build',
+            '-c',
+            'Release',
+            '-o',
+            `"${publishPath}"`
+        ]);
+
+        if (publishResult) {
+            core.setFailed('Publish failed.');
+            return;
+        }
+
+        core.info(`wwwroot Path: ${publishPath}/wwwroot`);
+        core.setOutput('wwwroot-path', `${publishPath}/wwwroot`);
+
+        if (context.repo.repo !== `${context.repo.owner}.github.io`) {
+            core.info('Modifying index.html for this repository...');
+            let indexFileContent = fs.readFileSync(`${publishPath}/wwwroot/index.html`, 'utf8');
+
+            indexFileContent = indexFileContent.replaceAll('base href="/"', `base href="/${context.repo.repo}/"`);
+
+            fs.writeFileSync(`${publishPath}/wwwroot/index.html`, indexFileContent);
+
+            if (fs.existsSync(`${publishPath}/wwwroot/404.html`)) {
+                core.info('Modifying 404.html for this repository...');
+                let fourFileContent = fs.readFileSync(`${publishPath}/wwwroot/404.html`, 'utf8');
+
+                fourFileContent = fourFileContent.replaceAll('/?p=/', `/${context.repo.repo}/?p=/`);
+
+                fs.writeFileSync(`${publishPath}/wwwroot/404.html`, fourFileContent);
+            }
+        }
+
+        //core.info('Creating .nojekyll...');
+        //fs.writeFileSync(`${publishPath}/wwwroot/.nojekyll`, '');
+    } catch (error: any) {
+        if (error instanceof Error) {
+            core.setFailed(error);
+        }
     }
-
-    core.info('Building...');
-    const buildResult = await exec.exec(dotnet, [
-      'build',
-      `"${projectPath}"`,
-      '--no-restore',
-      '-c',
-      'Release'
-    ]);
-
-    if (buildResult) {
-      throw new Error('Build failed.');
-    }
-
-    core.info('Publishing...');
-    const publishResult = await exec.exec(dotnet, [
-      'publish',
-      `"${projectPath}"`,
-      '--no-build',
-      '-c',
-      'Release',
-      '-o',
-      `"${publishPath}"`
-    ]);
-
-    if (publishResult) {
-      throw new Error('Publish failed.');
-    }
-
-    core.setOutput('wwwroot-path', `${publishPath}/wwwroot`);
-
-    if (!isMainRepo) {
-      core.info('Modifiyng index.html for this repository...');
-      const repo = await repoName();
-      let indexFileContent = fs.readFileSync(`${publishPath}/wwwroot/index.html`, 'utf8');
-
-      indexFileContent = indexFileContent.replaceAll('base href="/"', `base href="/${repo}/"`);
-
-      fs.writeFileSync(`${publishPath}/wwwroot/index.html`, indexFileContent);
-
-      if (fix404) {
-        core.info('Modifiyng 404.html for this repository...');
-        let fourFileContent = fs.readFileSync(`${publishPath}/wwwroot/404.html`, 'utf8');
-
-        fourFileContent = fourFileContent.replaceAll('/?p=/', `/${repo}/?p=/`);
-
-        fs.writeFileSync(`${publishPath}/wwwroot/404.html`, fourFileContent);
-      }
-    }
-
-    core.info('Creating .nojekyll...');
-    fs.writeFileSync(`${publishPath}/wwwroot/.nojekyll`, '');
-  } catch (error: any) {
-    core.setFailed(error.message);
-  }
 }
 
 main();
