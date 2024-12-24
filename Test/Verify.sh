9d5117789564bd2affd7f5040606b4d2111a92d8
Jonghyo Lee <na1307@outlook.kr>
2024-12-24 22:09:50 +0900

    test test

--- Test/Verify.sh
+++ Test/Verify.sh
@@ -1,50 +1,37 @@
 #!/usr/bin/bash
 
-#if [ $1 == "--main-repo=false" ]; then
-#    MAINREPO=false;
-#elif [ $1 == "--main-repo=true" ]; then
-#    MAINREPO=true;
-#else
-#    echo Unknown Argument.
-#    exit 1
-#fi
+verifyExitCode() {
+  if [ $2 -eq 0 ]; then
+    echo $1 verify passed.
+  else
+    echo $1 verify failed.
+    exit 1
+  fi
+}
 
-#if [ $2 == "--fix-404=false" ]; then
-#    FIX404=false;
-#elif [ $2 == "--fix-404=true" ]; then
-#    FIX404=true;
-#else
-#    echo Unknown Argument.
-#    exit 1
-#fi
-
-#if [ ! -f _out/wwwroot/.nojekyll ]; then
-#    echo ".nojekyll doesn't exist!"
-#    exit 1
-#fi
-
-#if [ $MAINREPO == true ]; then
-#    grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
-#else
-#    grep -i "base href=\"/$3/\"" _out/wwwroot/index.html > /dev/null
-#fi
-
-#if [ $MAINREPO == false ] && [ $FIX404 == true ]; then
-#    grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
-#else
-#    ! grep -i "/$3/?p=/" _out/wwwroot/404.html > /dev/null
-#fi
-
-grep -i "base href=\"/$1/\"" _out/wwwroot/index.html > /dev/null
-
-if [ $? -ne 0 ]; then
-  echo index.html verify failed.
+if [ $1 == "--main-repo=false" ]; then
+  MAINREPO=false;
+elif [ $1 == "--main-repo=true" ]; then
+  MAINREPO=true;
+else
+  echo Unknown Argument.
   exit 1
 fi
 
-grep -i "/$1/?p=/" _out/wwwroot/404.html > /dev/null
+if [ $MAINREPO == true ]; then
+  grep -i 'base href="/"' _out/wwwroot/index.html > /dev/null
+  verifyExitCode index $?
+else
+  grep -i "base href=\"/$2/\"" _out/wwwroot/index.html > /dev/null
+  verifyExitCode index $?
+fi
 
-if [ $? -ne 0 ]; then
-  echo 404.html verify failed.
-  exit 1
+if [ -e _out/wwwroot/404.html ]; then
+  if [ $MAINREPO == true ]; then
+    grep -i "/?p=/" _out/wwwroot/404.html > /dev/null
+    verifyExitCode 404 $?
+  elif [ $MAINREPO == false ]; then
+    grep -i "/$2/?p=/" _out/wwwroot/404.html > /dev/null
+    verifyExitCode 404 $?
+  fi
 fi
